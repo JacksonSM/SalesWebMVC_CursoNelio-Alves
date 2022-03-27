c72e28de4dfa1a411a3b9e31fe2af6145ded4fc0
JacksonS.Maciel <jacksons.m2018@gmail.com>
2022-03-27 14:49:24 -0300

    Other entities and second migration

---
+++ SalesWebMVC/Models/Identity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Identity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller : Identity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public Department Department { get; set; }
+
+        public Seller()
+        {
+        }
+
+        public Seller(string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        public void AddSales(SalesRecord sale)
+        {
+            Sales.Add(sale);
+        }
+        public void RemoveSales(SalesRecord sale)
+        {
+            Sales.Remove(sale);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+        
+
+    }
+}
